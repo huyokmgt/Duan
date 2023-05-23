e4e5116cc1c7cce0d35acaf733ae53f93ba6c4ab
huyokmgt <huyokmgt@gmail.com>
2023-05-23 09:32:00 +0700

    them ghi

--- app/src/main/java/huyvqph29932/fpoly/du_an_mau/MainActivity.java
+++ app/src/main/java/huyvqph29932/fpoly/du_an_mau/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         demoDB demoDB = new demoDB(getApplicationContext());
-        demoDB.thanhVien();
+        demoDB.thanhVien();/// gfgfg
     }
 }
